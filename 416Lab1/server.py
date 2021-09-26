bb7e4348da374ce5564ec147a608451a27c230ba
Daniel Astorino <35502486+astorinod@users.noreply.github.com>
2021-09-26 16:33:54 -0400

    Update server.py

--- 416Lab1/server.py
+++ 416Lab1/server.py
@@ -17,50 +17,68 @@ try:
 except:
     print("[ERROR] Missing required extension (sys)")
 
+try:
+    import os
+except:
+    print("[ERROR] Missing required extension (os)")
+
+
 # ---------------------------- ARGUMENTS -----------------------------------------
 arguments =  sys.argv
 host = '127.0.0.1' #localhost
 port = arguments[1]      
-#response = ""
+exists = True
 
 
-#Lab Requirement (i): Create a connection socket when contacted by the client 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+# ----------------------- Communicate with Client ---------------------------------
 
-    #bind socket to specified host and port
-    s.bind((host, int(port)))
-    #wait for connection
-    s.listen(5)
-    # connection established
-    conn, addr = s.accept()
-    print("Connection: OK")
+#Lab Requirement (i): Server is launched to listen to a localhost port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, int(port)))       #bind socket with a given host and port
+s.listen(5)                     #listen for a connection 
+conn, addr = s.accept()         #connection is established
+print("Connection: OK")
+
+
+with (conn):
     
-    with (conn):
-        dataPacket = conn.recv(1024)
-        print("Request Message recieved.")
+    #Lab Requirement (iii): Server receives an HTTP request from this connection 
+    dataPacket = conn.recv(1024)
+    print("Request Message recieved.")
 
-        filename = dataPacket.decode().split(" ")[-1]
-        if filename.endswith(".txt"):
-            content = "text/html"
-        if filename.endswith(".jpg"):
-            content = "image/jpg"
+    #Lab Requirement (iv): Request is parsed to determine path of a file being requested
+    filename = dataPacket.decode().split(" ")[-1]
+    
+    #We check the file type to determine the content type of the file
+    if filename.endswith(".txt"):
+        content = "text/html"
+    if filename.endswith(".jpg"):
+        content = "image/jpg"
 
-        try:
-            file = open(filename, 'rb')
-        except:
-            response = "404 Not found"
-            conn.sendall(response.encode()) 
-            s.close()
-        
+    #Lab Requirement (v): Server gets the requested file from the server's file system 
+    try:
+        file = open(filename, 'rb')
+    except:
+        #If the file does not exist in the server directory, send a 404 error response
+        response = "404 Not found"
+        exists = False
+        conn.sendall(response.encode()) 
+        s.close()
+    
+    #If the file exists we begin sending it to the client, packet by packet
+    if exists:
         try:
             stream = file.read(1024)
+            print("Sending...")
             while (stream):
-                print("Sending...")
                 conn.send(stream)
                 stream = file.read(1024)
             file.close()
-
+            
+            #Lab Requirement (vi): Server creates an HTTP response message incorporating a content of the 
+            #                      requested file preceded by header lines, including a responding code
             response =  "PUT / HTTP/1.1\r\nServer HTTP Reponse: HTTP 200 OK\r\nContent-Type: " + content
+            #Lab Requirement (vii): Server sends the response to the client
             conn.sendall(response.encode())
             print("Sending complete")
         except:
@@ -68,9 +86,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             conn.sendall(response.encode()) 
             s.close()
 
-
-        #print('Connected by', addr)
-
-#TODO Add timeout
-
-#404 and 200 doesnt work for images
+s.close()
