a56dab180e9eff7d116b05eb1f0f359861788f54
Daniel Astorino <35502486+astorinod@users.noreply.github.com>
2021-09-26 16:33:18 -0400

    Update client.py

--- 416Lab1/client.py
+++ 416Lab1/client.py
@@ -7,6 +7,8 @@ Daniel Astorino 260799820
 Lillian Chiu 
 '''
 # ---------------------------- IMPORTS -----------------------------------------
+
+
 try:
     import socket
 except:
@@ -17,63 +19,101 @@ try:
 except:
     print("[ERROR] Missing required extension (sys)")
 
+try:
+    import os
+except:
+    print("[ERROR] Missing required extension (os)")
+
+
 # ---------------------------- ARGUMENTS -----------------------------------------
 arguments = sys.argv
 host = '127.0.0.1'
 port = int(arguments[1])
 filename = arguments[2]
 
+try:
+    timeout = arguments[3]
+except:
+    timeout = 5
 
+response = ""
+deleteFile = False
 
+# ----------------------- Communicate with Server ---------------------------------
 
-#Lab Requirement (i): Create a connection socket to contact the server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((host, port))
-    print("Connection: OK")
+#Lab Requirement (ii): Client establishes a socket connection to the server
+#with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(timeout)
+s.connect((host, port))
+print("Connection: OK")
 
-    #Lab Requirement(ii): Send the HTTP request from this connection
+#Lab Requirement(iii): Send the HTTP request from this connection
+if filename.endswith(".txt"):
+    file = open('rx'+ filename, 'w', newline='')
+    content = "text/html"
+    request = "GET / HTTP/1.1\r\nContent-Type:text/html\n " + filename
+
+if filename.endswith(".jpg"):
+    file = open('rx'+ filename, 'wb')
+    content = "image/jpg"
+    request = "GET / HTTP/1.1\r\nContent-Type:image/jpeg\n " + filename
+
+s.sendall(request.encode())
+print("Request Message sent.")
+
+#Server gets request and starts sending
+
+data = s.recv(1024)
+print("Recieving...")
+while(data):
+    
+    #Check for a HTTP 200 response    
+    two = "200"
+    if filename.endswith(".txt") and ("200" in data.decode()):
+        response = "HTTP 200 OK"
+        break
+    if filename.endswith(".jpg") and (two.encode('utf-8') in data):
+        response = "HTTP 200 OK"
+        break
+    
+    #Check for a HTTP 404 response
+    four = "404"
+    if filename.endswith(".txt") and ("404" in data.decode()):
+        response = "HTTP 404 Not Found"
+        deleteFile = True
+        break
+    if filename.endswith(".jpg") and (four.encode('utf-8') in data):
+        response = "HTTP 404 Not Found"
+        deleteFile = True
+        break
+    
+    #If the data is not an ERROR or OK reponse, then it's file data. Write it in a new file
     if filename.endswith(".txt"):
-        file = open('rx'+ filename, 'w')
-        content = "text/html"
-        request = "GET / HTTP/1.1\r\nContent-Type:text/html\n " + filename
-
-    if filename.endswith(".jpg"):
-        file = open('rx'+ filename, 'wb')
-        content = "image/jpg"
-        request = "GET / HTTP/1.1\r\nContent-Type:image/jpeg\n " + filename
-
-    s.sendall(request.encode())
-    print("Request Message sent.")
-    
-    #Server gets request and starts sending
-    
+        file.write(data.decode())
+    elif filename.endswith(".jpg"):
+        file.write(data)
     data = s.recv(1024)
-    while(data):
-        print("Recieving...")
-        
-        #if(data.decode().startswith("404")):
-        #    print(data.decode())
-        #    break
-        
-        #if("200" in data.decode()):
-        #    break
+file.close()
 
-        if filename.endswith(".txt"):
-            file.write(data.decode())
-        elif filename.endswith(".jpg"):
-            file.write(data)
-        data = s.recv(1024)
+#If we recieved a 404 error code, make sure no new file is created
+if deleteFile and os.path.exists('rx'+ filename):
+    os.remove('rx'+ filename)
 
-    file.close()
-    print("Server HTTP Reponse: HTTP 200 OK")
+#Print out the output response, and the file content if the file is a txt
+print("Server HTTP Reponse: " + response)
+
+if os.path.exists('rx'+ filename):
     print("Content-Type: " + content)
-    
     if content == "text/html":
         file = open('rx'+filename)
         text = file.read()
         print(text)
-    
-    #print("data packet: ", data.decode())
+
+try:
+    s.close()
     print("Socket Closed")
+except:
+    print("Closing Socket Failed")
 
 
